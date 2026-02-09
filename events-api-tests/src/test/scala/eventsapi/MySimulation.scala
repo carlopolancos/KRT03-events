5ad27a7895ec7d5514ba5c83eee3cd3b750b3fc5
Carlo James Polancos <carlo.polancos@awsys-i.com>
2026-02-09 14:43:06 +0800

    Updated repo

---
+++ events-api-tests/src/test/scala/eventsapi/MySimulation.scala
@@ -0,0 +1,22 @@
+package eventsapi
+
+import com.intuit.karate.gatling.PreDef._
+import io.gatling.core.Predef._
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class MySimulation extends Simulation {
+
+  val getEvents =
+    scenario("load-events")
+      .exec(karateFeature("classpath:eventsapi/events/publicevents.feature"))
+
+  val auth =
+    scenario("authentication")
+      .exec(karateFeature("classpath:eventsapi/events/authentication.feature"))
+
+  setUp(
+    getEvents.inject(rampUsers(100) during (5)),
+    auth.inject(rampUsers(500) during (5))
+  )
+}
